7995d5458d812dd7edc44ada07873e84dadc0f29
Busi <pietersonbusi@gmail.com>
2023-07-20 19:33:36 +0200

    docker-compoe added

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/todos", async (req, res) => {
+  console.log("hello")
   const todos = await Todo.find();
   return res.status(200).json({
     todos,
@@ -45,7 +46,7 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 mongoose.connect(
-  "mongodb://mongodb:27017/todos-app",
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/todos-app?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
